db45b796100e1489c9ef1dea98c8f0a800ef1d07
Joe de Casanove <jlvdecasanove@gmail.com>
2024-04-01 16:37:30 +0100

    changed detectPalindrome function  to use string concatenation rather than converting into an array.

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,34 @@
 detectPalindrome = (str) => {
+    
+    /*
+    //Old method: converting to an array
     let strArray = [];
     for (let i = str.length -1; i >= 0; i--) {
         strArray.push(str.slice(i, i + 1));
     }
     comparisonStr = strArray.join('');
-    console.log("Flipping word...")
+    console.log("Flipping word...");
     console.log(`Flipped word: ${comparisonStr}`);
     if (str === comparisonStr) {
         return true;
     } else {
         return false;
     }
+    */
+
+    //New method: concatinating a string
+    console.log("Flipping word...");
+    let comparisonStr = "";
+    for (let i = str.length -1; i>= 0; i--) {
+        comparisonStr += str.charAt(i);
+    }
+    console.log(`Flipped word: ${comparisonStr}`);
+    if (str === comparisonStr) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 console.log("Enter a word to check if it's a palindrome");
